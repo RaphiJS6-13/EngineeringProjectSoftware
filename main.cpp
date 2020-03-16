0f823457106fb832c3f314cbc1f1269d7feadda1
RaphiJS6-13 <53946354+RaphiJS6-13@users.noreply.github.com>
2020-03-16 12:42:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <Servo.h>
-#include ""
+#include "SwingMachine.hpp"
 
 
 void setup() {
+	for (int count = (int) FIRSTPIN; count < (int) FIRSTPIN + 7; count++) {
+		pinMode(count, OUTPUT);
+	}
 	core::init();
 	core::run();
+	core::deinit();
 }
 
-// 🙄 (dead, required code)
+// 🙄 
 void loop() {}
