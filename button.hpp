ea3ca553c2af2fc72770acaf6a204c48fc485a9c
RaphiJS6-13 <53946354+RaphiJS6-13@users.noreply.github.com>
2020-04-22 18:45:32 -0400

    Create button.hpp

---
+++ button.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace button {
+  const int BUTTON_PIN = 1; // this might change
+  
+  void init() {
+    pinMode(BUTTON_PIN, INPUT);
+  }
+  
+  inline bool isPressed() {
+    return digitalRead(BUTTON_PIN) == HIGH;
+  }
+  
+  bool waitForPress(void (* callback)())
+  {
+    while (!isPressed())
+      delay();
+    
+    (*callback)();
+    
+    bool held;
+    
+    do {
+      held = isPressed();
+    } while(held);
+    
+  }
+}
