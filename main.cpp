43fab21e3802786a27ecc6539db63439b9575821
RaphiJS6-13 <53946354+RaphiJS6-13@users.noreply.github.com>
2020-04-22 18:56:17 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <Servo.h>
-#include <memory>
 #include "SwingMachine.hpp"
 
 
@@ -8,13 +7,16 @@ void setup()
 	int first = core::FIRST_PIN;
 	int last = first + 6; // six motors
 	
-	for(int pin = first; pin <= last; pin++) {
+	for(int pin = first; pin <= last; pin++)
 		pinMode(pin, OUTPUT);
-	}
 	
 	core::init();
-	std::shared_ptr _ <void> (NULL, [&](...){ code::deinit(); });
-	core::run();
+
+	for(int i = 0; i < 50; i++) {
+		core::run();
+	}
+	
+	core::deinit();
 }
 
 // 🙄 
